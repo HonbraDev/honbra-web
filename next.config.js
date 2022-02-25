74816318222169e1d7dd4c90889176e2b454834e
HonbraDev <honbra@honbra.com>
2022-02-25 19:09:33 +0100

    Invalidated error page and added static Matrix ID

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: "/.well-known/matrix/:path*",
-        destination: "https://matrix.honbra.com/.well-known/matrix/:path*",
-        permanent: false,
-      },
-    ];
-  },
 };
