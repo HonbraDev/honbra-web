c26e6054e6a1ff51afdd908d5d99040d3f3b69f4
Honbra <honbraofficial@gmail.com>
2022-07-19 01:11:02 +0200

    Minehut changed variables and I need to push.

--- src/modules/minehut-servers/meta.ts
+++
@@ -1,9 +0,0 @@
-import PageMeta from "../../types/PageMeta";
-
-const meta: PageMeta = {
-  title: "Minehut servers",
-  route: "/minehut-servers",
-  description: "A simple Minehut server visualizer"
-};
-
-export default meta;
